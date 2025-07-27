fix diam calculation

src/aneurysm_sim/model/model.py
@@ -167,7 +167,7 @@ def simulate_aneurysm(params, genotype = None, treatment = False, dt = 0.0069): 
     lambda_c_max[0] = params.c_lambda_sys / lambda_rec_min[0]
     lambda_c_min[0] = params.c_lambda_sys / lambda_rec_max[0]
     lambda_c_mode[0] = params.c_lambda_sys/  lambda_rec_mode[0]
-    diameter[0] = 2*params.c_radius_tzero*params.c_lambda_sys*1e3
+    diameter[0] = 2*params.c_radius_tzero*params.c_lambda_sys
     tau[0] = 4 * params.c_flow_rate * params.c_blood_viscosity / (np.pi * (diameter[0]/2)**3)  # Shear stress in Pa
     tau_homeo = tau[0]
     lambda_sys_array[0] = params.c_lambda_sys
@@ -195,7 +195,7 @@ def simulate_aneurysm(params, genotype = None, treatment = False, dt = 0.0069): 
         lambda_rec_max[i] = lambda_rec_max[i-1] + dt * d_collagen_max_recruitment_stretch_ad_dt(alpha, lambda_c_min[i], lambda_att_min[i])
         lambda_rec_mode[i] = lambda_rec_mode[i-1] + dt * d_collagen_mode_recruitment_stretch_ad_dt(alpha, lambda_c_mode[i], lambda_att_mode[i])
 
-        diameter[i] = 2 * params.c_radius_tzero * lambda_sys * 1e3
+        diameter[i] = 2 * params.c_radius_tzero * lambda_sys
         tau[i] = 4 * params.c_flow_rate * params.c_blood_viscosity / (np.pi * (params.c_radius_tzero * 1e3 * lambda_sys)**3)  # Shear stress in Pa
         # print(tau)
         eta = 1.0